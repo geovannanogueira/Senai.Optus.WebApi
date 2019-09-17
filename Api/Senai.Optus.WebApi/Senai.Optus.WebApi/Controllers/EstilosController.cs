c8718d38b4e49635068fb41ebc7ca55aa02feab1
geovannanogueira <geovannat.nogueira@gmail.com>
2019-09-17 15:57:19 -0300

    irra

---
+++ Api/Senai.Optus.WebApi/Senai.Optus.WebApi/Controllers/EstilosController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Optus.WebApi.Domains;
+using Senai.Optus.WebApi.Repositories;
+
+namespace Senai.Optus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class EstilosController : ControllerBase
+    {
+        EstiloRepository EstilosRepository = new EstiloRepository();
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(EstilosRepository.Listar());
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpPost]
+        public IActionResult Cadastrar(Estilos estilo)
+        {
+            try
+            {
+                EstilosRepository.Cadastrar(estilo);
+                return Ok();
+            }
+            catch (Exception ex )
+            {
+                return BadRequest(new { mensagem = "Erro ao Cadastrar." + ex.Message });
+            }
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Estilos Estilo = EstilosRepository.BuscarPorId(id);
+            if (Estilo == null)
+                return NotFound();
+            return Ok(Estilo);
+
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpPut]
+        public IActionResult Atualizar (Estilos estilo)
+        {
+            try
+            {
+                Estilos EstiloBuscada = EstilosRepository.BuscarPorId(estilo.IdEstilo);
+                if (EstiloBuscada == null)
+                    return NotFound();
+                EstilosRepository.Atualizar(estilo);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Deu Erro ai" + ex.Message });
+            }
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            EstilosRepository.Deletar(id);
+            return Ok();
+        }
+    }
+}
---
+++ Api/Senai.Optus.WebApi/Senai.Optus.WebApi/Repositories/EstiloRepository.cs
@@ -0,0 +1,59 @@
+﻿using Senai.Optus.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Repositories
+{
+    public class EstiloRepository
+    {
+        public List<Estilos> Listar()
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Estilos.ToList();
+            }
+        }
+
+        public void Cadastrar (Estilos estilo)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                ctx.Estilos.Add(estilo);
+                ctx.SaveChanges();
+            }
+        }
+
+        public Estilos BuscarPorId(int id)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Estilos.FirstOrDefault(x => x.IdEstilo == id);
+            }
+        }
+
+        public void Atualizar (Estilos estilo)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                Estilos EstilosBuscada = ctx.Estilos.First(x => x.IdEstilo == estilo.IdEstilo);
+ 
+                EstilosBuscada.Nome = estilo.Nome;
+                ctx.Estilos.Update(EstilosBuscada);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                Estilos Estilo = ctx.Estilos.Find(id);
+                ctx.Estilos.Remove(Estilo);
+                ctx.SaveChanges();
+            }
+        }
+
+    }
+}
